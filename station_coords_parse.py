ede4ccd312fee417d9435e0c578d9e4475b9a4a1
caped-doshi <arvind27kr@gmail.com>
2022-02-10 22:31:26 -0500

    implemented distance function to remove data inconsistencies

--- station_coords_parse.py
+++ station_coords_parse.py
@@ -23,7 +23,7 @@ class Station_Coords:
                 point = point.split(' ')
                 lat = float(point[1])
                 lon = float(point[0])
-                lines = row[4].split('-')
+                lines = list(row[4].split('-'))
                 self.station_data.append([name, (lat,lon), lines])
                 if name in self.index_map:
                     self.index_map[name].append((index, lines))
@@ -40,7 +40,7 @@ class Station_Coords:
                     n = row[1]
                     lat = float(row[2])
                     lon = float(row[3])
-                    temp_tuple = self.find_station_index(n, lat, lon)
+                    temp_tuple = self.find_station_index(alt_id, lat, lon)
                     index = temp_tuple[0]
                     name = temp_tuple[1]
                     lines = temp_tuple[2]
@@ -55,6 +55,9 @@ class Station_Coords:
                     j+=1
             print(f"num less than 3: {j}")
 
+    def dist(self, lat1: float, lon1: float, lat2: float, lon2: float):
+        return ((lat2-lat1)**2 + (lon2-lon1)**2)**.5
+
     #given a station and a line, return station id
     def get_station_id(self, station: str, line: str) -> int:
         stations = self.index_map[station]
@@ -66,24 +69,27 @@ class Station_Coords:
 
     #given lat and lon return index
     def find_station_index(self, name: str, lat: float, lon: float) -> int:
-        EPSILON = 0.001
+        EPSILON = 0.0005
+        to_return = 1
+        min_dist = 1000
+        temp_name = ""
+        l = []
         for i in range(len(self.station_data)):
-            digits_lat = Decimal(str(lat))
-            digits_lat = -1*digits_lat.as_tuple().exponent
-            digits_lon = Decimal(str(lon))
-            digits_lon = -1*digits_lon.as_tuple().exponent
             station = self.station_data[i]
-            temp_lat = float(station[1][0])
-            temp_lon = float(station[1][1])
-            temp_lat = round(temp_lat, digits_lat)
-            temp_lon = round(temp_lon, digits_lon)
-            n = station[0]
-            lines = station[2]
-            if abs(lat - temp_lat) < EPSILON and abs(lon - temp_lon) < EPSILON \
-                or name == station[0]:
-                #print(f"name: {name}, found_station: {n}")
-                return (i, n, lines)
-        return (-1, "", lines)
+            if len(station) < 4:
+                temp_lat = float(station[1][0])
+                temp_lon = float(station[1][1])
+                n = station[0]
+                lines = station[2]
+                temp_dist = self.dist(lat, lon, temp_lat, temp_lon)
+                if temp_dist < min_dist:
+                    min_dist = temp_dist
+                    to_return = i
+                    temp_name = n
+                    l = lines
+                    # print(f"name: {name}, found_station: {n}")
+                    # print(f"lat1: {lat}, lat2: {temp_lat}, lon: {lon}, lat2: {temp_lon}")
+        return (to_return, temp_name, l)
 
         #given an alt_id, e.x 119S, return the tuple(index, lines, name)
     def get_station_by_alt_id(self, alt_id: str):
@@ -102,8 +108,17 @@ class Station_Coords:
         station = self.get_station_by_alt_id(alt_id)
         return station[0][2]
 
+    def create_csv(self):
+        print(self.station_data[0])
+        f = open('master_data.csv', 'w', newline='')
+        writer = csv.writer(f)
+        writer.writerow(['Name', '(Latitude, Longitude)', 'Lines','Alternate ID'])
+        for row in self.station_data:
+            writer.writerow(row)
+
 #alt_id map format as follows (index, lines, name)
 
 sc = Station_Coords("stationCords.csv", "stop_locations.csv")
 print(f"index: {sc.get_index_by_alt_id('119S')}")
 print(f"name: {sc.get_name_by_alt_id('119S')}")
+sc.create_csv()
